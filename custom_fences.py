c7a09872481d91b3a6e1e22e9c1e1634980debe5
Michael Droschak <mdroschak@cleavelandprice.com>
2024-05-20 07:06:32 -0400

    Remove unused code

--- custom_fences.py
+++ custom_fences.py
@@ -124,24 +124,3 @@ def httpCall(source, language, class_name, options, md, **kwargs):
     
     # Return the formatted HTML
     return html
-
-
-        # <span class="opblock-summary">Headers</span>
-        # <span class="twemoji toggle-symbol">
-        #     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        #         <path d="M18 8H8v10H6V8a2 2 0 0 1 2-2h10v2m-4-6H4a2 2 0 0 0-2 2v10h2V4h10V2m8 10v8a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2v-8a2 2 0 1 1 2-2h8a2 2 0 0 1 2 2m-2 3h-3v-3h-2v3h-3v2h3v3h2v-3h3v-2Z"></path>
-        #     </svg>
-        # </span>
-
-        # <input type="checkbox" id="toggleHeaders_{input_id}" class="toggle-input"> 
-
-        #             <label for="toggleHeaders_{input_id}" class="opblock-summary-control">
-        #         <div class="opblock-section-request">
-        # <div class="opblock-body">
-
-        #         </div>
-        #     </label>
-        #     <div class="opblock-request-body">
-        #         <pre class="body-param"><code class="lang-json">{headers.strip()}</code></pre>
-        #     </div>
-        # </div>
